ae0c580f22b3f0fe48dbb75d932e4b7042dc6c9a
00014812 <115162785+00014812@users.noreply.github.com>
2022-11-03 21:32:05 +0500

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -37,4 +37,46 @@ class BodySegment(Block):
     def move(self, newPos):
         self.pos=newPos
 
-class
+class WallSegment(Block):
+
+    def __init__(self, pos):
+        Block. __init__(self)
+        self.color = pygame.Color(200, 200, 200)
+        self.pos = pos
+
+class Apple(Black):
+
+    def __init__(self):
+        Block.__init__(self)
+        self.color = pygame.color(255, 0, 0 )
+        self.randomize()
+
+    #Place apple in random block position
+    def randomize(self):
+
+        for i, widthOrHeight in enumerate(self.pos):
+            self.pos[i] = random.choice(range(SQUARE_BLOCK_SIZE, SCREEN_W_H[0] - SQUARE_BLOCK_SIZE,))
+
+class Chainable:
+
+    def draw(self, screen):
+        for seg in self.segments:
+            seg.draw(screen)
+
+class Wall(Chainable):
+
+    def __init__(self):
+        self.segments = []
+
+    #Make segments form around borders
+
+       for x in range(0, SCREEN_W_H[0], SQUARE_BLOCK_SIZE):
+          self.segments.append( WallSegment([x, 0]))
+          self.segments.append( WallSegment([x, SCREEN_W_H[1] - SQUARE_BLOCK_SIZE]))
+
+       for y in range(SQUARE_BLOCK_SIZE, SCREEN_W_H[1] - SQUARE_BLOCK_SIZE):
+          self.segments.append(WallSegment([0, y]))
+          self.segments.append(WallSegment([x, SCREEN_W_H[1] - SQUARE_BLOCK_SIZE, y]))
+
+    def checkWallCollision(self, snake):
+
