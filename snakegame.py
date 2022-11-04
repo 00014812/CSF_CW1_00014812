e8ef27e60636aed0662c1cdd154e17edf10b8f11
00014812 <115162785+00014812@users.noreply.github.com>
2022-11-04 20:39:23 +0500

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -80,3 +80,46 @@ class Wall(Chainable):
 
     def checkWallCollision(self, snake):
 
+        rext1 = Rect(snake.head.pos[0], snake.head.pos[1], SQUARE_BLOCK_SIZE, SQUARE_BLOCK_SIZE)
+
+        for seg in self.segments:
+            rect2 = Rect(seg.pos[0], seg.pos[1], SQUARE_BLOCK_SIZE, SQUARE_BLOCK_SIZE)
+            if checkCollision(rect1, rext2)
+                return True
+
+        return False
+
+class Snake(Chainable):
+
+    def __init__(self):
+        self.direction = 'right'  #Default to moving right
+        self.moveDist = SQUARE_BLOCK_SIZE * Move_SPEED
+        self.segments = []
+        self.size = 0
+        for i in range(2): #Place segments in top left corner
+            if i == 0:
+                self.segments.append(BodySegment([SQUARE_BLOCK_SIZE,200])) #2 body segments to start
+                self.head = self.segments[0] #Set the head segment for the rest of the game
+            else:
+                self.segments.append( BodySegment([0, 200]))
+
+    def changeDir(self, direction):
+
+        if self.direction == 'right' and direction == 'left':
+            pass
+        elif self.direction == 'left' and direction == 'right':
+            pass
+        elif self.direction == 'up' and direction == 'down':
+            pass
+        elif self.direction == 'down' and direction == 'up':
+            pass
+        else:
+            self.direction = direction
+
+    def checkTailCollision(self):
+
+        for i, seg in enumerate(self.segments):
+            if i == 0:
+                continue
+            rect1 = Rect(self.head.pos[0], self.head.pos[1], SQUARE_BLOCK_SIZE, SQUARE_BLOCK_SIZE)
+            rect2 =
